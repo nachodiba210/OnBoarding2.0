78f59e601d3993145585007ae70c4db321a5ae26
nachodiba210 <nachodiba210@hotmail.com>
2020-07-29 12:55:02 -0300

    Complete
    js and jquery complete

--- Onboarding-JS/index.js
+++ Onboarding-JS/index.js
@@ -1,60 +1,50 @@
+
 let modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-let btnAdd = document.getElementById("add-button");
+let btnAdd = document.getElementById("add-button").addEventListener('click', togglePopUp);
 
 let table = document.getElementById("myTable");
 
-let btnOptimize = document.getElementById("optimize-button");
+let btnOptimize = document.getElementById("optimize-button").addEventListener('click', optimizerDeal);
 
-let create = document.getElementById("create-button");
+let create = document.getElementById("create-button").addEventListener('click',createNewItem);
 
-
+window.addEventListener('click',windowPopUp);
 
 // When the user clicks the button, open the modal
-btnAdd.onclick = function showPopUp() {
-  modal.classList.add("d-block");
+function togglePopUp() {
+  modal.classList.toggle("d-block");
+}
+
+
+function windowPopUp(event) {
+  if (event.target == modal) {
+    modal.classList.toggle('d-block');
+  }
 }
 
 
 // Optimize Button on click function gets the best deal for your budget!!
-btnOptimize.onclick = function optimizerDeal() {
-  let text = [];
-  let result = 0;
-  let budget = (document.getElementById("budget").textContent).substr(8);
+function optimizerDeal() {
+  let budget = parseInt(document.getElementById('budget').textContent.substr(1));
   let rowLength = table.tBodies[0].rows.length;
-  for (let i = 1; i < rowLength + 1; i++) {
-    let idCheckbox = "checkbox" + (i);
-    table.rows[i].cells[2].firstChild.setAttribute("id", idCheckbox);
-    if (document.getElementById(idCheckbox).checked) {
-      document.getElementById(idCheckbox).checked = false;
-    }
-  }
-  // get all the cost of each element and sorts them out.
-  for (let i = 1; i < rowLength + 1; i++) {
-    let costRow = parseInt((table.rows[i].cells[1].innerHTML).substr(1), 10);
-    text.push(costRow);
-  }
-  text.sort((a, b) => a - b);
-  // goes through all the elements and start checking the elements you got to buy
-  for (let i = 0; i < text.length; i++) {
-    result = result + text[i];
-    if (result <= budget) {
-      for (let j = 1; j < rowLength + 1; j++) {
-        let costRow = parseInt((table.rows[j].cells[1].innerHTML).substr(1), 10);
-        if (text[i] == costRow) {
-          let idCheckbox = "checkbox" + (j);
-          document.getElementById(idCheckbox).checked = true;
-          continue;
-        }
-      }
-    }
-  }
+  let rows = table.rows;
   sortTable();
+  // goes through all the elements and start checking the elements you got to buy
+  for (let i = rowLength; i >= 1; i--) {
+    let costRow = parseInt((rows[i].cells[1].innerHTML).substr(1), 10);
+    let budgetLessCost = budget-costRow;
+    if (budgetLessCost >= 0) {
+      budget -= costRow;
+      rows[i].cells[2].childNodes[0].checked = true;
+    }
+  }
 }
 
+
 // When the user clicks on "create", close the modal
-create.onclick = function createNewItem() {
+function createNewItem() {
   // Find a <table> element with id="myTable"
   let name = document.getElementById("fname").value;
   let cost = document.getElementById("cost").value;
@@ -70,42 +60,41 @@ create.onclick = function createNewItem() {
   nameCell.innerHTML = name;
   costCell.innerHTML = ('$' + cost);
   checkBoxCell.append(checkBox);
-  modal.classList.remove("d-block");
+  modal.classList.toggle("d-block");
 }
 
 // function that is hable to sort the rows of a table using the cost column
 function sortTable() {
   let i, costFirstRow, costNextRow;
-  let swtich;
+  let Switch;
   let switching = true;
 
   // Run loop until no switching is needed
   while (switching) {
-      switching = false;
-      let rows = table.rows;
+    switching = false;
+    let rows = table.rows;
 
-      // Loop to go through all rows
-      for (i = 1; i < (rows.length - 1); i++) {
-          Switch = false;
+    // Loop to go through all rows
+    for (i = 1; i < (rows.length - 1); i++) {
+      Switch = false;
 
-          // Fetch 2 elements that need to be compared
-          costFirstRow = parseInt((table.rows[i].cells[1].innerHTML).substr(1), 10);
-          costNextRow = parseInt((table.rows[i+1].cells[1].innerHTML).substr(1), 10);
+      // Fetch 2 elements that need to be compared
+      costFirstRow = parseInt((rows[i].cells[1].innerHTML).substr(1), 10);
+      costNextRow = parseInt((rows[i + 1].cells[1].innerHTML).substr(1), 10);
 
-          // Check if 2 rows need to be switched
-          if (costFirstRow > costNextRow)
-              {
+      // Check if 2 rows need to be switched
+      if (costFirstRow < costNextRow) {
 
-              // If yes, mark Switch as needed and break loop
-              Switch = true;
-              break;
-          }
+        // If yes, mark Switch as needed and break loop
+        Switch = true;
+        break;
       }
-      if (Switch) {
-          // Function to switch rows and mark switch as completed
-          rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-          switching = true;
-      }
+    }
+    if (Switch) {
+      // Function to switch rows and mark switch as completed
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
   }
 }
 
---
+++ Onboarding-Jquery/index.js
@@ -0,0 +1,92 @@
+
+// When the user clicks the button, open the modal
+$("#add-button").click(function showModal() {
+  $("#myModal").addClass("d-block");
+});
+
+// Touch any part of the windows when adding a new item and the modal will close.
+$('#myModal').click(function (event){
+   if($(event.target).is('#myModal')){
+     $('#myModal').removeClass('d-block');
+   }
+});
+
+// Optimize Button on click function gets the best deal for your budget!!
+$("#optimize-button").click(function optimizerTable() {
+  let budget = parseFloat(($('#budget').text()));
+  let rowLength = 0;
+  let tbody = $('#myTable tbody');
+  $("#myTable tbody tr").each(function () { rowLength++ });
+  sortTable();
+  $("#myTable tbody tr").each(function () {
+    let costRow = $(this).find('td:nth-child(2)').text().substr(1);
+    let budgetLessCost = budget - costRow;
+    if (budgetLessCost >= 0) {
+      budget -= costRow;
+      $(this).find('input').prop('checked', true);
+    }
+  });
+  tbody.html($('tr',tbody).get().reverse());
+});
+
+// When the user clicks on "create", close the modal
+$("#create-button").click(function createNewItem() {
+  // Find a <table> element with id="myTable"
+  let name = $("#fname").val();
+  let cost = $("#cost").val();
+  console.log(name);
+  console.log(cost);
+
+  $("#myTable").find('tbody')
+    .append($('<tr>')
+      .append($('<td>')
+        .text(name)
+      )
+      .append($('<td>')
+        .text("$" + cost)
+      )
+      .append($('<td>')
+        .append($(document.createElement('input')).attr({
+          class: 'form-check-input mx-auto',
+          type: 'checkbox'
+        }))
+      )
+    );
+  $("#myModal").removeClass("d-block");
+});
+
+//function that is hable to sort the rows of a table using the cost column
+function sortTable() {
+  let costFirstRow, costNextRow;
+  let Switch;
+  let rowToChange;
+  let switching = true;
+
+  // Run loop until no switching is needed
+  while (switching) {
+    switching = false;
+    let rows = $('#myTable tbody tr');
+
+    // Goes to all rows except last and ask if the next row has a higher cost than the one on
+    // if it does it put switch in true and after it changes.
+    (rows.not(":last")).each(function iterateRows(e) {
+      Switch = false;
+      costFirstRow = parseFloat(($(this).find('td:nth-child(2)')).text().substr(1));
+      costNextRow = parseFloat(($(this).next('tr').find('td:nth-child(2)')).text().substr(1));
+      if (costFirstRow > costNextRow) {
+        rowToChange = $(this);
+        Switch = true;
+        return false;
+      }
+    });
+    // this if is able to change the rows if they have to change.
+    if(Switch){
+      rowToChange.insertAfter(rowToChange.next('tr'));
+      switching = true;
+    }
+  }
+}
+
+
+
+
